5eb4ef6095bfa9c9089cbb60c598df8124982e27
1gust4 <gustavo09022005@gmail.com>
2022-03-25 16:40:50 -0300

    aula2503 pam

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Cabecalho from './src/componentes/cabecalho';
+
+export default function App() {
+  return (
+    <View>
+
+      <Cabecalho/>
+
+    </View>
+  );
+}
+
+
